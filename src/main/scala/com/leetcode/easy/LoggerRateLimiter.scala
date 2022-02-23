827c448288feccb6bdf2a32369bdc8cbc61afd2c
kumar Ankit <ankitax@gmail.com>
2022-02-23 23:13:23 +0700

    better log rate limiter

--- src/main/scala/com/leetcode/easy/LoggerRateLimiter.scala
+++ src/main/scala/com/leetcode/easy/LoggerRateLimiter.scala
@@ -1,5 +1,9 @@
 package com.leetcode.easy
 
+import scala.collection.mutable.Seq
+import scala.collection.mutable.HashSet
+import scala.collection.mutable.ListBuffer
+
 object LoggerRateLimiter extends App {
   class Logger {
     val map = scala.collection.mutable.Map[String, Int]()
@@ -16,19 +20,49 @@ object LoggerRateLimiter extends App {
       }
       isAllowed
     }
+
+    case class Log(timestamp: Int, message: String) // DS to store log
+
+    var list = new scala.collection.mutable.ListBuffer[Log]() // to keep timestamp
+    var messages = scala.collection.mutable.HashSet[String]() // to keep unique message in last 10 unit of time
+
+    def shouldPrintMessageUpdated(timestamp: Int, message: String): Boolean = {
+
+      //first we do clean up - GC
+      while (!list.isEmpty && timestamp - list.headOption.map(_.timestamp).getOrElse(Int.MinValue) >= 10) {
+        /*
+          remove all the message from head of list which are older than 10 unit of time
+          also clean HashSet
+         */
+        messages.remove(list.head.message)
+        list = list.drop(1)
+      }
+      if (messages.contains(message)) false
+      else {
+        list += Log(timestamp, message)
+        messages.add(message)
+        true
+      }
+    }
   }
 
 
   val logger = new Logger()
-  logger.shouldPrintMessage(1, "foo") // return true, next allowed timestamp for "foo" is 1 + 10 = 11
+  println(logger.shouldPrintMessageUpdated(1, "foo")) // return true, next allowed timestamp for "foo" is 1 + 10 = 11
 
-  logger.shouldPrintMessage(2, "bar") // return true, next allowed timestamp for "bar" is 2 + 10 = 12
+  println(logger.shouldPrintMessageUpdated(2, "bar")) // return true, next allowed timestamp for "bar" is 2 + 10 = 12
 
-  logger.shouldPrintMessage(3, "foo") // 3 < 11, return false
+  println(logger.shouldPrintMessageUpdated(3, "foo")) // 3 < 11, return false
 
-  logger.shouldPrintMessage(8, "bar") // 8 < 12, return false
+  println(logger.shouldPrintMessageUpdated(8, "bar")) // 8 < 12, return false
 
-  logger.shouldPrintMessage(10, "foo") // 10 < 11, return false
+  println(logger.shouldPrintMessageUpdated(11, "foo")) // 10 < 11, return false
 
-  logger.shouldPrintMessage(11, "foo")
+  println(logger.shouldPrintMessageUpdated(11, "foo"))
+  println(logger.shouldPrintMessageUpdated(32, "foo1"))
+  println(logger.shouldPrintMessageUpdated(32, "foo2"))
+  println(logger.shouldPrintMessageUpdated(32, "fo3o"))
+  println(logger.shouldPrintMessageUpdated(32, "fo54o"))
+  println(logger.shouldPrintMessageUpdated(32, "fo6o"))
+  println(logger.shouldPrintMessageUpdated(32, "fo7o"))
 }
