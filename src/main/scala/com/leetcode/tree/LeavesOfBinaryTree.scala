28498679b174896e4ce14ae06a5d31a137d4f344
Kumar Ankit <ankitax@gmail.com>
2022-05-14 22:24:32 +0530

    height to btree, removing leaves

--- src/main/scala/com/leetcode/tree/LeavesOfBinaryTree.scala
+++ src/main/scala/com/leetcode/tree/LeavesOfBinaryTree.scala
@@ -2,21 +2,53 @@ package com.leetcode.tree
 
 import com.leetcode.easy.TreeNode
 
+import scala.collection.mutable.ListBuffer
+
 object LeavesOfBinaryTree extends App {
-  val array: List[List[Int]] = List(List())
 
   def findLeaves(root: TreeNode): List[List[Int]] = {
-    if (root == null) List(List())
-    else {
-      dfs(List(root))
-    }
+    val bfsList: ListBuffer[ListBuffer[Int]] = ListBuffer.empty
 
-    def dfs(tree: List[TreeNode]): Unit = {
-      val nextNode = List[TreeNode]()
-      tree.map {
-        n => ???
+    def getHeight(root: TreeNode): Int = {
+      if (root == null) -1
+      else {
+        val height = 1 + Math.max(getHeight(root.left), getHeight(root.right))
+        if (bfsList.isEmpty || bfsList.size <= height)
+          bfsList.insert(height, ListBuffer(root.value))
+        else
+          bfsList(height).addOne(root.value)
+        height
+
       }
     }
-    array
+
+    getHeight(root)
+    bfsList.toList.map(_.toList)
+
   }
+
+
+  /*  def bfs(root: TreeNode, height: Int, leaf: ListBuffer[Int], bfsList: ListBuffer[ListBuffer[Int]]): Unit = {
+      if (root == null) {}
+      else if (root.left == root.right && root.left == null) {
+        leaf += root.value
+      }
+      else {
+        if (bfsList.isEmpty || bfsList.size <= height) {
+          bfsList.insert(height, ListBuffer(root.value))
+        }
+        else {
+          bfsList(height).addOne(root.value)
+        }
+        bfs(root.left, height + 1, leaf, bfsList)
+        bfs(root.right, height + 1, leaf, bfsList)
+      }*/
+
+
+
+
+
+  println(findLeaves(TreeNode(37,
+    TreeNode(-34, null, TreeNode(-100)),
+    TreeNode(-48, TreeNode(-100), TreeNode(48, TreeNode(54, TreeNode(71), TreeNode(22, null, TreeNode(8))))))))
 }
